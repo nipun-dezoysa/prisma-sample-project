bb69768e33e7bee8a622547e1be7c3e8b38ac8b5
nipun-dezoysa <nipunavishka123@gmail.com>
2024-07-25 16:35:56 +0530

    insert and select data

---
+++ script.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+    const user = await prisma.user.create({data: {name: "Nipun"}});
+    const all  = await prisma.user.findMany();
+    console.log(all); 
+}
+
+main()
+  .catch((e) => {
+    console.log(e.message);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
